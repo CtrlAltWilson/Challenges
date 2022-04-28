85f19a89eba841850b1b7e0a357037371d655760
CtrlAltWilson <72035077+CtrlAltWilson@users.noreply.github.com>
2022-04-27 20:40:14 -0500

    Update Problem 14.py

--- Project Euler/Problem 14.py
+++ Project Euler/Problem 14.py
@@ -2,7 +2,8 @@
 """
 https://projecteuler.net/problem=14
 The following iterative sequence is defined for the set of positive integers:
-n → n/2 (n is even)n → 3n + 1 (n is odd)
+n → n/2 (n is even)
+n → 3n + 1 (n is odd)
 Using the rule above and starting with 13, we generate the following sequence:
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
 It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
@@ -18,14 +19,27 @@ start_time = time.time()
 clear = lambda: os.system('cls')
 clear()
 
-def problem14(num):
-    return 0
+def problem14(limit):
+    largest = larCount = 0
+    for i in range(2,limit,1):
+        num = i
+        counter = 0
+        while num != 1:
+            if num%2 == 0:
+                num /= 2
+            else:
+                num = 3*num+1
+            counter +=1
+        if counter > larCount:
+            largest = i
+            larCount = counter
+    return "Starting chain {} has the longest chains at {} chains".format(largest,larCount)
 
-num = 0
-print(problem14(num))
+limit = 1000000
+print(problem14(limit))
 
 print("--- {} seconds ---".format(time.time() - start_time))
 print("\n\n")
 
-#Answer is
+#Answer is 837799
 
